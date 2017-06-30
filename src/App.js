97a73d409d826700952cd24b8f02f5d30845d65c
davidandrus <davidandrus.com@gmail.com>
2017-06-30 09:15:09 -0700

    adding react-router

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import './App.css'
 import SearchPage from './SearchPage';
 import BooksList from './BooksList';
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
+class BooksApp extends React.Component {
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage 
-          ? <SearchPage />
-          : <BooksList />
-        }
-      </div>
+      <Router>
+        <div className="app">
+          <Route
+            exact
+            path="/"
+            component={BooksList}
+          />
+          <Route
+            exact
+            path="/search"
+            component={SearchPage}
+          />
+        </div>
+      </Router>
     )
   }
 }
