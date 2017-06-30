ed7a2816d96a03cdcaceb58c491f3039e758329e
davidandrus <davidandrus.com@gmail.com>
2017-06-30 10:21:15 -0700

    getting some api calls happening

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,20 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { search } from './BooksAPI';
 
-export default function SearchPage() {
-  return (
-    <div className="search-books">
-      <div className="search-books-bar">
-        <a
-          className="close-search" 
-          onClick={() => this.setState({ showSearchPage: false })}
-        >Close</a>
-        <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author"/>
+class SearchPage extends Component {
+  state = {
+    searchTerm: '',
+    resultsMessage: '',
+    searchResults: [],
+  };
+
+  _handleSearchChange = (e) => {
+    e.preventDefault();
+    const searchTerm = e.target.value;
+    const searchTermTrimmed = searchTerm.trim();
+    this.setState({ searchTerm });
+
+    // @TODO - only make sure most recent promise resolves
+    // @TODO - add loading indicator
+    search(searchTermTrimmed, 20)
+      .then(searchResults => {
+        if (Array.isArray(searchResults)) {
+          this.setState({
+            searchResults,
+          });
+        } else {
+          this.setState({
+            searchResults: [],
+          });
+        }
+      });
+  }
+
+  render() {
+    const {
+      searchResults,
+      searchTerm,
+    } = this.state;
+
+    const resultsMessage = searchTerm.trim() && !searchResults.length
+      ? 'No Results Found for Query'
+      : 'Enter a Search Query';
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <a
+            className="close-search" 
+            onClick={() => this.setState({ showSearchPage: false })}
+          >Close</a>
+          <div className="search-books-input-wrapper">
+            <input
+              onChange={this._handleSearchChange}
+              type="text"
+              placeholder="Search by title or author"
+              value={searchTerm}
+            />
+          </div>
+        </div>
+        <div className="search-books-results">
+          {JSON.stringify({searchResults})}
+          {resultsMessage &&
+            <p>{resultsMessage}</p>
+          }
+          <ol className="books-grid"></ol>
         </div>
       </div>
-      <div className="search-books-results">
-        <ol className="books-grid"></ol>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default SearchPage;
